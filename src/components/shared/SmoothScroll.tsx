f85c0917bd7342b503f791f6562517a4a55c4221
David Yang <davidyang@crc-dot1x-nat-10-239-146-46.bu.edu>
2026-04-29 12:24:54 -0400

    tweak: increase scroll smoothness duration to 1.8

--- src/components/shared/SmoothScroll.tsx
+++ src/components/shared/SmoothScroll.tsx
@@ -6,7 +6,7 @@ export function SmoothScroll() {
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.8,
       easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       touchMultiplier: 2,
       infinite: false,
