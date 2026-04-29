bf532a1e07a1b648514d9bc89cbbc3aa7bd9c39d
David Yang <davidyang@crc-dot1x-nat-10-239-146-46.bu.edu>
2026-04-29 12:27:51 -0400

    tweak: scroll duration to 1.3

--- src/components/shared/SmoothScroll.tsx
+++ src/components/shared/SmoothScroll.tsx
@@ -6,7 +6,7 @@ export function SmoothScroll() {
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.5,
+      duration: 1.3,
       easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       touchMultiplier: 2,
       infinite: false,
